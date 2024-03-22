3da84fbeb2ee9e6dbc6c7caa943e402cc5be90c8
Igor Martins <igor.martins@ibridge.com.br>
2024-03-22 14:41:06 -0300

    created all database relations

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -4,6 +4,9 @@ import { UpdateItemDto } from './dto/update-item.dto';
 import { EntityManager, Repository } from 'typeorm';
 import { Item } from './entities/item.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Listing } from './entities/listing.entity';
+import { Comment } from './entities/comment.entity';
+import { Tag } from './entities/tag.entity';
 
 @Injectable()
 export class ItemsService {
@@ -11,10 +14,25 @@ export class ItemsService {
     @InjectRepository(Item)
     private readonly itemsRepository: Repository<Item>,
     private readonly entityManager: EntityManager
-  ) { }
+  ) {}
 
   async create(createItemDto: CreateItemDto) {
-    const item = new Item(createItemDto);
+    const listing = new Listing({
+      ...createItemDto.listing,
+      rating: 0
+    });
+
+    const tags = createItemDto.tags.map(
+      (createTagDto) => new Tag(createTagDto)
+    );
+    
+    const item = new Item({
+      ...createItemDto,
+      comments: [],
+      tags,
+      listing
+    });
+
     await this.entityManager.save(item);
   }
 
@@ -23,13 +41,22 @@ export class ItemsService {
   }
 
   findOne(id: number) {
-    return this.itemsRepository.findOneBy({ id });
+    return this.itemsRepository.findOne({
+      where: { id },
+      relations: { listing: true, comments: true, tags: true }
+    });
   }
 
   async update(id: number, updateItemDto: UpdateItemDto) {
     const item = await this.itemsRepository.findOneBy({ id });
 
     item.public = updateItemDto.public;
+
+    const comments = updateItemDto.comments.map(
+      (createCommentDto) => new Comment(createCommentDto)
+    );
+
+    item.comments = comments;
 
     await this.entityManager.save(item);
   }
