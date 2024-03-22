f685698be74b99fb5d0936a79ed4591565de88e5
Igor Martins <igor.martins@ibridge.com.br>
2024-03-22 13:06:32 -0300

    created items api routes + logics

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,26 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
+import { EntityManager, Repository } from 'typeorm';
+import { Item } from './entities/item.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class ItemsService {
-  create(createItemDto: CreateItemDto) {
-    return 'This action adds a new item';
+  constructor(
+    @InjectRepository(Item)
+    private readonly itemsRepository: Repository<Item>,
+    private readonly entityManager: EntityManager
+  ) { }
+
+  async create(createItemDto: CreateItemDto) {
+    const item = new Item(createItemDto);
+    await this.entityManager.save(item);
   }
 
   findAll() {
-    return `This action returns all items`;
+    return this.itemsRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} item`;
+    return this.itemsRepository.findOneBy({ id });
   }
 
-  update(id: number, updateItemDto: UpdateItemDto) {
-    return `This action updates a #${id} item`;
+  async update(id: number, updateItemDto: UpdateItemDto) {
+    const item = await this.itemsRepository.findOneBy({ id });
+
+    item.public = updateItemDto.public;
+
+    await this.entityManager.save(item);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} item`;
+  async remove(id: number) {
+    return this.itemsRepository.delete(id);
   }
 }
